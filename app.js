62df2bfeda81b98507119c44151bfd96e7ceb464
Tia Holmes <tia2019@live.unc.edu>
2019-10-29 16:58:08 -0400

    end of day

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var bodyParser=require('body-parser');
 var jsonParser=bodyParser.json();
 var urlencodedParser= bodyParser.urlencoded({ extended: false});
-
+var famMems=require('./info.json');
 // fs.readFile('./info.json', 'utf8', (err, jsonString) => {
 //     if (err) {
 //         console.log("Error reading file from disk:", err)
@@ -21,27 +21,38 @@ var urlencodedParser= bodyParser.urlencoded({ extended: false});
 
 app.set('view engine', 'ejs');
 var port = process.env.PORT || 3000;
-app.use('/assets', express.static(__dirname +'/public'));
+app.use(express.static(__dirname +'/public'));
 app.get('/', function(req, res) {
+    console.log(famMems);
     res.render('index');
+   
 });
 app.get('/aboutme', function(req, res) {
     res.render('aboutme');
+    
 });
 app.get('/food', function(req, res) {
     res.render('food');
+
 });
 app.get('/contact', function(req, res) {
     res.render('contact');
+   
 });
 app.get('/family', function(req, res) {
-    res.render('family');
+    res.render('family', {famMems:famMems}); 
+    
 });
 app.post('/contact', urlencodedParser,function(req, res){
     console.log(req.body.firstname);
     console.log(req.body.email);
     console.log(req.body.comment);
    
+    res.send("recieved your request!");
+ });
+ app.post('/family', urlencodedParser,function(req, res){
+    var responseName= req.body.dropnames;
+   console.log(responseName);
     res.send("recieved your request!");
  });
 app.listen(port);
