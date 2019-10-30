0e47efdb9f0554ae6663a328f041a289d8f9c322
Tia Holmes <tia2019@live.unc.edu>
2019-10-30 15:58:34 -0400

    wed afternoon

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var bodyParser=require('body-parser');
 var jsonParser=bodyParser.json();
 var urlencodedParser= bodyParser.urlencoded({ extended: false});
 var famMems=require('./info.json');
-// fs.readFile('./info.json', 'utf8', (err, jsonString) => {
+var mysql=require('mysql');
+// fs.readFile('./info.json', 'utf8', (err, jsonString) => 
 //     if (err) {
 //         console.log("Error reading file from disk:", err)
 //         return
@@ -37,32 +38,64 @@ app.get('/food', function(req, res) {
 });
 app.get('/contact', function(req, res) {
     res.render('contact');
+
    
 });
 app.get('/family', function(req, res) {
     res.render('family', {famMems:famMems}); 
     
 });
-app.get('/katie', function(req, res) {
-    res.render('katie'); 
+
+app.post('/contact', urlencodedParser,function(req, res, next){
+    var firstname=req.body.firstname;
+    var email=req.body.email;
+    var comment =req.body.comment;
+    console.log(req.body)
+    if(firstname==='' || email===''|| comment==='') {
+        res.status(400)
+        res.render('error','FAILED TO ADD TO DATABASE, missing valueERROR')
+
+    } else {
+
+        res.send("recieved your request!");
+    }
+
+
+    var con = mysql.createConnection({
+        host:"localhost",
+        user:"root",
+        password:"password",
+        database:"sys"
+    });
+    con.connect();
+    var qury = `INSERT INTO sys.contactform (comment, email, submission_date, firstname ) VALUES ( "${comment}", "${email}", CURDATE(), "${firstname}" )`;
+    console.log(qury);
+    con.query(qury, 
+        function(err){
+            if(err) {
+                res.status(500)
+                res.render('error','FAILED TO ADD TO DATABASE, MYSQL ERROR')
+
+            }
+            //console.log(rows[0]);
+        }
     
-})
-app.post('/contact', urlencodedParser,function(req, res){
-    console.log(req.body.firstname);
-    console.log(req.body.email);
-    console.log(req.body.comment);
-   
-    res.send("recieved your request!");
+    );
+
+    con.end();
+    next();
  });
  app.post('/family', urlencodedParser,function(req, res){
     var responseName= req.body.dropnames;
    console.log(responseName);
-    res.redirect('/family/'+responseName);
+    res.redirect('/familydetail/'+responseName);
  });
 
- app.get('/familydetail:responseName', function(req, res) {
+ app.get('/familydetail/:responseName', function(req, res) {
     var responseName=req.params.responseName;
-    res.render('family', {name:responseName}); 
+    var person= famMems.filter(item=>item.Name===responseName);
+    console.log(person[0].Name);
+    res.render('familydetail', {person:person[0]}); 
     
 });
 app.listen(port);
